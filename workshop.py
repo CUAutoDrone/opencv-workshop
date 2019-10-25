b2ef77af4061b2844fe07e57a4e4454058e1e3d3
rpappa <rjpappa@gmail.com>
2019-10-25 13:17:18 -0400

    added basis code

---
+++ workshop.py
@@ -0,0 +1,28 @@
+import cv2
+import numpy as np
+
+targets = [
+    ("Image 1", cv2.imread("img/1.jpg"), 4, 4),
+    ("Image 2", cv2.imread("img/2.jpg"), 2, 4),
+    ("Image 3", cv2.imread("img/3.jpg"), 6, 8)
+]
+
+for target in targets:
+    name = target[0]
+    img = target[1]
+    minFind = target[2]
+    maxFind = target[3]
+
+    found = -1
+
+    # Todo: your vision pipeline here
+    # update the variable found to equal the number
+    # of pumpkins in [img]
+
+    # uncomment this line to pass one of the test cases
+    # found = 3
+
+    if found < minFind or found > maxFind:
+        print("FAILED :( " + name)
+    else:
+        print("PASSED :D " + name)
